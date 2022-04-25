9f4591898eac261959337d2d8efccf2c7bf0c843
2288-256 <74705442+2288-256@users.noreply.github.com>
2022-04-25 19:04:43 +0900

    認証コードに関する修正

--- js/authcode.js
+++ js/authcode.js
@@ -13,16 +13,15 @@ function textcopy() {
 			.map((v) => v.toString(16).padStart(2, "0"))
 			.join("");
 	}
-	var AuthCode = document.getElementById("AuthCode");
 	var today = new Date();
 	var year = today.getFullYear();
 	var month = today.getMonth() + 1;
 	var day = today.getDate();
 	var date1 = year + "" + month + "" + day;
-	var test2;
 	anpieo(date1).then((hash) =>
 		saoi(hash).then((text) => {
 			navigator.clipboard.writeText(text);
 		})
 	);
+	alert("認証コードをコピーしました");
 }
