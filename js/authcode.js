a09504f02226d7348e45beef33c58b72b4e16164
2288-256 <74705442+2288-256@users.noreply.github.com>
2022-05-01 16:26:01 +0900

    認証コードの修正

--- js/authcode.js
+++ js/authcode.js
@@ -1,3 +1,26 @@
+async function saoi(text) {
+	const uint8 = new TextEncoder().encode(text);
+	const digest = await crypto.subtle.digest("SHA-1", uint8);
+	return Array.from(new Uint8Array(digest))
+		.map((v) => v.toString(16).padStart(2, "0"))
+		.join("");
+}
+async function anpieo(text) {
+	const uint8 = new TextEncoder().encode(text);
+	const digest = await crypto.subtle.digest("SHA-256", uint8);
+	return Array.from(new Uint8Array(digest))
+		.map((v) => v.toString(16).padStart(2, "0"))
+		.join("");
+}
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+var date1 = year + "" + month + "" + day;
+anpieo(date1).then((hash) =>
+	saoi(hash).then((AuthCodes) => (AuthCode.innerHTML = AuthCodes))
+);
+
 function textcopy() {
 	async function saoi(text) {
 		const uint8 = new TextEncoder().encode(text);
@@ -18,14 +41,10 @@ function textcopy() {
 	var month = today.getMonth() + 1;
 	var day = today.getDate();
 	var date1 = year + "" + month + "" + day;
-	var AuthCode = document.getElementById("AuthCode");
 	anpieo(date1).then((hash) =>
 		saoi(hash).then((text) => {
 			navigator.clipboard.writeText(text);
 		})
-	);
-	anpieo(date1).then((hash) =>
-		saoi(hash).then((AuthCodes) => (AuthCode.innerHTML = AuthCodes))
 	);
 	alert(
 		"認証コードをコピーしました\nMacの方は以下のリンクでご確認ください\nhttps://guide.zpw.jp/playerworld/AuthCodes"
